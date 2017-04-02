36d631452900c921d31e8117da08c0ded16489fd
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-04-02 14:15:42 +0530

    Basic CSS styling using framework.

--- src/cljs/app/views.cljs
+++ src/cljs/app/views.cljs
@@ -8,26 +8,35 @@
 (defn search-results
   "Displays search results in a Reactable Table."
   [results]
-  (let [displayed-results (mapv #(dissoc % :ragam-id) results)]
-    [:> Table {:data displayed-results}]))
+  [:div.column
+   (when (not (empty? results))
+     [:> Table {:data results
+                :className "table is-striped"
+                :columns [{:key :name        :label "Name"}
+                          {:key :arohanam    :label "Arohanam"}
+                          {:key :avarohanam  :label "Avarohanam"}
+                          {:key :ragam-link  :label "Wiki Page"}
+                          {:key :data-source :label "Source"}]}])])
 
 (defn search-bar
   "Displays the search bar and the search button."
-  [query]
-  [:div [:input {:type :text
-                 :value query
-                 :placeholder "Enter ragam name"
-                 :on-change #(re-frame/dispatch [:search/change-query (-> % .-target .-value)])}]
-   [:button {:on-click #(re-frame/dispatch [:search/ragams! query])}
-    "Search Ragams"]])
+  [query loading?]
+  [:div.field.has-addons.level-item.column.is-6.is-offset-3
+   [:input.input {:type :text
+                  :value query
+                  :placeholder "Enter ragam name"
+                  :on-change #(re-frame/dispatch [:search/change-query (-> % .-target .-value)])}]
+   [:button.button
+    {:class (if loading? :is-loading :is-primary)
+     :on-click #(re-frame/dispatch [:search/ragams! query])}
+    "Search"]])
 
 (defn search []
   (let [query (re-frame/subscribe [:search/query])
         results (re-frame/subscribe [:search/results])
         loading? (re-frame/subscribe [:loading])]
     (fn []
-      [:div
-       (search-bar @query)
-       (if @loading?
-         [:p "Loading..."]
-         (search-results @results))])))
+      [:div.columns
+       [:div.column.is-offset-2.is-8
+        (search-bar @query @loading?)
+        (search-results @results)]])))
