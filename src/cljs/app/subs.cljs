5250d7aa9feaf245101a6d875cac20110e07282f
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-04-05 13:11:12 +0530

    Add subscription for ragam-page retrieved data.

--- src/cljs/app/subs.cljs
+++ src/cljs/app/subs.cljs
@@ -7,7 +7,8 @@
   {:name "re-frame"
    :loading false
    :search {:query ""
-            :results []}})
+            :results []}
+   :ragam  {:loading false}})
 
 (defn- loading
   [db]
@@ -25,9 +26,14 @@
   [{db :search}]
   (:results db))
 
+(defn- ragam-data
+  [{db :ragam}]
+  (:data db))
+
 (defn init
   []
   (re-frame/reg-sub :loading loading)
   (re-frame/reg-sub :active-panel active-panel)
   (re-frame/reg-sub :search/query search-query)
-  (re-frame/reg-sub :search/results search-results))
+  (re-frame/reg-sub :search/results search-results)
+  (re-frame/reg-sub :ragam/data ragam-data))
